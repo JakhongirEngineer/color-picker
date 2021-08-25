5d76719e49e08ecc44747332849930499f67df37
jakhongirrasulov007 <jakhongirrasulov001@gmail.com>
2021-08-26 00:58:29 +0500

    creates Palette component

---
+++ src/Palette.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Palette(props) {
+  return (
+    <div className="Palette">
+      {/* header */}
+      <div className="Palette-colors">Palette colors</div>
+      {/* footer */}
+    </div>
+  );
+}
+
+export default Palette;
