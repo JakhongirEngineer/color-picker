8c94214d7c2d60f3b9c0ef5eff79d95a4afc5ffa
jakhongirrasulov007 <jakhongirrasulov001@gmail.com>
2021-08-29 00:00:32 +0500

    adds react-router to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./colorHelpers";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 function App() {
   console.log();
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[1])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list</h1>} />
+      <Route exact path="/palette/:id" render={() => <h1>Palette</h1>} />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[1])} />
+    // </div>
   );
 }
 
