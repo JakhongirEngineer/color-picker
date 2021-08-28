a560605d3a33b3aef95e45424212015c0332e0e4
jakhongirrasulov007 <jakhongirrasulov001@gmail.com>
2021-08-29 00:01:24 +0500

    creates Navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "rc-slider/assets/index.css";
+import Slider from "rc-slider";
+import { MenuItem, Select } from "@material-ui/core";
+
+import "./Navbar.css";
+
+function Navbar({ level, setLevel, format, setFormat }) {
+  return (
+    <header className="Navbar">
+      <div className="logo">
+        <a href="#">ReactColorPicker</a>
+      </div>
+
+      <div className="slider-container">
+        <span>Level: {level}</span>
+        <div className="slider">
+          <Slider
+            value={level}
+            min={100}
+            max={900}
+            step={100}
+            onChange={(val) => setLevel(val)}
+          />
+        </div>
+      </div>
+      <div className="select-container">
+        <Select
+          onChange={(e) => setFormat(e.target.value)}
+          defaultValue="hex"
+          value={format}
+        >
+          <MenuItem value="hex">HEX</MenuItem>
+          <MenuItem value="rgb">RGB</MenuItem>
+          <MenuItem value="rgba">RGBA</MenuItem>
+        </Select>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
